9fda37ab3b572b2d36ffb461d5fe014e9bd5a07e
loulou123546 <loulou.valleres@live.fr>
2018-07-30 15:50:51 +0200

    add mouse support

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 require("code/utils")
+require("code/mouse")
 --require("code/map") seem useless
 require("code/level")
 
 function love.load()
     love.window.setTitle("Le Bouc Fou")
     utils.load()
+    utils.setScreen(800,600,true)
+    mouse.load()
 --    map.load()
     level.start()--must be the last to load
 end
